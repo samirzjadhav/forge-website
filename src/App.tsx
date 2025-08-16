fe5bbd7a17309d1ab1c8c7cf3cafc30186ca5c86
samirzjadhav <samirzjadhav@gmail.com>
2025-08-16 23:16:50 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-blue-500">
+        Hello Tailwind 3.4 + Vite 🚀
+      </h1>
+    </>
+  );
+}
+
+export default App;
